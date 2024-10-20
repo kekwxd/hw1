9d913d085fdf5574ff0d359a47236cda4e281ab9
kekwxd <78672158+kekwxd@users.noreply.github.com>
2024-10-20 18:50:56 +0300

    %100 hali
    baştan değiştirdim düzeldi oh be

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ using namespace std;
 
 int main(){
     string yes, doner, fries, ayran, baklava;
-    float indirimli = 0, hesapla = 0, indirimsiz = 0, donern = 0, friesn = 0, ayrann = 0, baklavan = 0, indirimsizsizayran = 0;
+    double indirimli = 0, hesapla = 0, indirimsiz = 0, donern = 0, friesn = 0, ayrann = 0, baklavan = 0, indirimsizsizayran = 0;
     bool combo = false;
     bool discount = false;
     
@@ -32,17 +32,7 @@ int main(){
     if (yes == "yes"){
         cout << "How many Doner Kebabs do you want? ";
         cin >> donern;
-        doner = donern;
-        if (donern >= 5)          
-        {
-            
-            combo = true;
-          
-        }
-        indirimli = indirimli + donern*200;
-        indirimsiz += (donern*200);
-
-        
+        doner = donern;   
     }
     
     //cout << indirimsiz-indirimli << endl;
@@ -52,11 +42,7 @@ int main(){
     if (yes == "yes"){
         cout << "How many French Fries do you want? ";
         cin >> friesn;
-        fries = friesn;
-        
-        
-        //indirimsiz += (hesapla*72);
-        
+        fries = friesn;   
     }
     //cout << indirimsiz-indirimli << endl;
     //cout << indirimli << endl;
@@ -65,113 +51,64 @@ int main(){
     if (yes == "yes"){
         cout << "How many Ayrans do you want? ";
         cin >> indirimsizsizayran;
-        ayrann = indirimsizsizayran;
-        indirimli = indirimli + ayrann*30;
-        
-        if (combo == true)
-        {   
-            
-            if (!(donern > 0) || !(friesn > 0) || !(indirimsizsizayran > 0)){
-                //cout << ayrann << endl;
-                ayrann = ayrann + 2;
-                //cout << ayrann << endl;
-                discount = true;
-                
-
-                discount = true;
-                if (friesn > 0){
-                    indirimli = indirimli + ((friesn*72)*0.75);
-                    indirimsiz += (friesn*72);
-
-                }
-                
-                
-                 
-                ////cout << indirimli << endl;
-            }
-            else
-            {   
-                if (friesn > 0){
-                    indirimli = indirimli + (friesn*72);
-                    indirimsiz += (friesn*72);
-
-                }
-                
-                
-                
-            
-            }
-        }
-        else
-        {   
-            if (friesn > 0){
-                indirimli = indirimli + (friesn*72);
-                indirimsiz += (friesn*72);
-            }
-            
-            
-            
-        
-        }    
-        
-    
-        
-        
-        
-        indirimsiz = indirimsiz + (ayrann*30);
-        
-      
+        ayrann = indirimsizsizayran; 
     }
     //cout << indirimsiz-indirimli << endl;
     //cout << indirimli << endl;
     cout << "Do you want Baklava? ";
-    cin >> yes;
-    
+    cin >> yes;    
     if (yes == "yes"){
         cout << "How many Baklavas do you want? ";
         cin >> baklavan;
         //baklava = baklavan;
-        
-
     }
     bool sonunda = false;
+
+    double uclu = donern*200 + friesn*72 + indirimsizsizayran*30;
+    indirimsiz += baklavan*219;
+    indirimsiz += uclu;
+    
     //cout << indirimli << endl;
     if (donern > 0 && friesn > 0 && indirimsizsizayran > 0 ){
         sonunda = true;
         //cout << indirimli << endl;
-        indirimli = indirimli*0.80;
+        indirimli = uclu*0.20;
+        
         //cout << indirimli << endl;
+        
     }
     
-    if (baklavan < 4 && (baklavan > 0)) {
-        indirimli = indirimli + (baklavan*219);
-        indirimsiz = indirimsiz + (baklavan*219);
-    }
-    if (sonunda) {
-        hesapla = 0;
-    }
+    else if (donern >= 5){
+        ayrann+=2;
+        indirimsiz+= 60;
+        indirimli += 60;
+        if (friesn > 0){
+            indirimli+= friesn*72*0.15;
+        }
 
+
+    }
+    
     else if (baklavan >= 4){
 
-        indirimli = indirimli + ((baklavan*219)*0.9);
-        indirimsiz = indirimsiz + (baklavan*219);
-        discount = true;
+        indirimli = indirimli + ((baklavan*219)*0.1);
+        
             
     }
     
     
     
     else if (indirimsiz >= 1500){
-        indirimli= indirimli- (indirimli*0.05);
+        indirimli= indirimli + indirimsiz*0.05;
         //cout << "Cha12312nge: " << indirimsiz << " TL" << endl;
         
     }
     
     cout << "Enter the amount of money the customer has: ";
     cin >> hesapla;
-    
-    
-    if (hesapla >= indirimli){
+    double idal = 0;
+    idal = indirimsiz-indirimli;
+    if (hesapla >= (idal)){
         cout << endl;
         cout << "--- Invoice Summary ---" << endl;
         if (donern > 0){
@@ -194,27 +131,23 @@ int main(){
             
             
         }
-        float idal = 0;
+        
        
-        idal = hesapla-indirimli;
-        cout << "Total Discount: " << indirimsiz-indirimli << " TL" << endl;
-        cout << "Total Cost: " << hesapla-idal << " TL" << endl;
+        
+        cout << "Total Discount: " << indirimli << " TL" << endl;
+        cout << "Total Cost: " << indirimsiz-indirimli << " TL" << endl;
         cout << "Amount Paid: " << hesapla << " TL" << endl;
         
         
-        cout << "Change: " << idal << " TL" << endl;
+        cout << "Change: " << hesapla-idal << " TL" << endl;
         
-        
-        
-       
-    
+
     }
-    
-    
+
     else{
         
         cout << "Unfortunately, you do not have enough money to complete this order.";
-        cout << " You are missing: " << indirimli-hesapla << " TL";
+        cout << " You are missing: " << idal-hesapla << " TL";
     }
     //cout << "Thank you for using the Restaurant Order Program!" << endl;
     return 0;
